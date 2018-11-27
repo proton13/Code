d030ba56f584697adef25d061891b9a10cbb7cdb
GUO Yichen <e0220147@u.nus.edu>
2018-11-27 16:50:57 +0800

    NLSW1

---
+++ NLSW/ode_main.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import ode_solver
+
+lbd = 1
+alp = 2
+omg = 0.1
+u0 = 2
+T = 4
+
+lve = np.array([2**1, 2**2, 2**3, 2**4, 2**5, 2**6, 2**7, 2**8, 2**9])
+#lve = np.array([2**1])
+ve = 1. / lve
+nve = lve.size
+
+lvt = 10*np.array([10**4, 2**1, 2**2, 2**3, 2**4,
+                   2**5, 2**6, 2**7, 2**8, 2**9])
+#lvt = np.array([10**4])
+vt = 1. / lvt
+nvt = lvt.size
+
+error_y = np.zeros((nve, nvt - 1))
+ratio = np.zeros((nve, nvt - 2))
+
+for ie in range(nve):
+    eps = ve[ie]
+    v0 = 1j * (-lbd**2 * u0 - np.abs(u0)**2 * u0) + eps**alp * omg
+    for it in range(nvt):
+        t = vt[it]
+        (x, u, p) = ode_solver.solver(lbd, T, eps, t, u0, v0)
+        '''
+        print(x[-1])
+        plt.plot(x, p, label="eps=" + str(eps))
+        if(it == 1):
+        plt.plot(x, p, label="tau=" + str(t))
+        plt.legend()
+        '''
+        if it == 0:
+            st = u
+        else:
+            error_y[ie, it - 1] = np.abs(st[-1] - u[-1])
+        if it >= 2:
+            ratio[ie, it - 2] = np.log(error_y[ie, it - 2] /
+                                       error_y[ie, it - 1]) / np.log(vt[it - 1] / vt[it])
+
+
+# plt.show()
+print('ratio')
+print(ratio)
+print('error')
+print(error_y)
---
+++ NLSW/ode_solver.py
@@ -0,0 +1,63 @@
+import numpy as np
+import math
+
+
+def solver(lbd, T, eps, t, u0, v0):
+
+    iA = np.array([[1, 0], [0, 1], [0, 0]])
+    iB = np.array([[-1, 0], [0, -1]])
+    n = int(round(T / t)) + 1
+    x = np.zeros(n)
+    u = np.zeros(n, dtype=complex)
+    v = np.zeros(n, dtype=complex)
+    p = np.zeros(n)
+
+    bp = (1 + np.sqrt(1 + 4 * eps**2 * lbd**2)) / (2 * eps**2)
+    bm = (-2 * lbd**2) / (1 + np.sqrt(1 + 4 * eps**2 * lbd**2))
+    b = bp - bm
+
+    I = np.zeros((5, 5), dtype=complex)
+    for Ip in range(5):
+        for Im in range(5):
+            tb = bp * (Ip - 2) + bm * (Im - 2)
+            if tb != 0:
+                I[Ip, Im] = 2 * np.exp(1j * tb * t / 2) * \
+                    np.sin(tb * t / 2) / tb
+            else:
+                I[Ip, Im] = t
+
+    u[0] = u0
+    v[0] = v0
+    p[0] = u[0].real
+    print(n)
+    for k in range(n):
+        x[k] = k * t
+    for k in range(1, n):
+        gmm = -(bm * u[k - 1] + 1j * v[k - 1]) / b
+        nu = (bp * u[k - 1] + 1j * v[k - 1]) / b
+        d = u[k - 1] * u[k - 1].conjugate() * u[k - 1] / (eps**2 * b)
+        A = np.array([gmm + d / bp, nu - d / bm, -
+                      d / bp + d / bm], dtype=complex)
+        B = np.array(
+            [[np.exp(1j * bp * t), 1j * bp * np.exp(1j * bp * t)], [-np.exp(1j * bm * t), -1j * bm * np.exp(1j * bm * t)]])
+        C = np.zeros((5, 5, 2), dtype=complex)
+
+        for ai1 in range(3):
+            for ai2 in range(3):
+                for ai3 in range(3):
+                    for bi in range(2):
+                        ci = iA[ai1, 0] - iA[ai2, 0] + iA[ai3, 0] + iB[bi, 0] + 2
+                        cj = iA[ai1, 1] - iA[ai2, 1] + iA[ai3, 1] + iB[bi, 1] + 2
+                        C[ci, cj, 0] += A[ai1] * A[ai2].conjugate() * A[ai3] * \
+                            B[bi, 0]
+                        C[ci, cj, 1] += A[ai1] * A[ai2].conjugate() * A[ai3] * \
+                            B[bi, 1]
+
+        [u1i, u2i] = np.tensordot(C, I, ((0, 1), (0, 1)))
+
+        u[k] = gmm * np.exp(1j * bp * t) + nu * np.exp(1j *bm * t) + u1i * 1j / (eps**2 * b)
+        v[k] = gmm * np.exp(1j * bp * t) * 1j * bp + nu * np.exp(1j * bm * t) * 1j * bm + u2i * 1j / (eps**2 * b)
+
+        p[k] = u[k].real
+
+    return (x, u, p)
